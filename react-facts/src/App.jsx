af0b70e1db4562fde644743f246e99e12779d8b8
moo-farah <rizmik.farah@gmail.com>
2026-09-14 15:01:05 +0300

    first commit

---
+++ react-facts/src/App.jsx
@@ -0,0 +1,19 @@
+import reactLogo from './assets/react.svg'
+const App = () => {
+  return (
+    <div>
+        <img src={reactLogo} alt="" width="40px" />
+        <h1>Fun facts about React</h1>
+        <ul>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100k stars on Github</li>
+          <li>Is maintained by Meta</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
+  
+  )
+}
+
+export default App
