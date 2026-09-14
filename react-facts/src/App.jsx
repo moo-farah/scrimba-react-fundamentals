84247d4e6ca0fa920cd239573b4e1477ce243529
moo-farah <rizmik.farah@gmail.com>
2026-09-14 19:17:10 +0300

    refactor: split layout into header, main, and footer

--- react-facts/src/App.jsx
+++ react-facts/src/App.jsx
@@ -1,19 +1,58 @@
 import reactLogo from './assets/react.svg'
+
+function Header () {
+  return (
+    <header>
+      <img 
+        src={reactLogo} 
+        alt="react-logo" 
+        width="40px" />
+
+        <nav>
+          <ul className='nav'>
+            <li className='nav-item'>Pricing</li>
+            <li className='nav-item'>About</li>
+            <li className='nav-item'>Careers</li>
+          </ul>
+        </nav>
+    </header>
+  )
+}
+
+function MainContent() {
+  return (
+    <main>
+    <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on Github</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      <small>
+        <p>© {new Date().getFullYear()} Scrimba Course Development. All rights reserved.</p>
+      </small> 
+    </footer>
+  )
+}
 const App = () => {
   return (
-    <div>
-        <img src={reactLogo} alt="" width="40px" />
-        <h1>Fun facts about React</h1>
-        <ul>
-          <li>Was first released in 2013</li>
-          <li>Was originally created by Jordan Walke</li>
-          <li>Has well over 100k stars on Github</li>
-          <li>Is maintained by Meta</li>
-          <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </div>
+    <>
+    <Header />
+    <MainContent />
+    <Footer />
+    </>
   
   )
 }
+
 
 export default App
